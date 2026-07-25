eb67e00e07710fe0255bcc65b4c1c72eff1c0167
Angel Szymczak <contact@angelpixel.io>
2026-07-25 16:55:48 -0300

    feat: backfill users and memberships from accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_25_150000) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_25_152000) do
   create_table "accounts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", null: false
