c3222d89e202aa9a2bcb19b5bb11cba07153c2b1
Angel Szymczak <contact@angelpixel.io>
2026-07-29 05:43:10 -0300

    fix: resolve packwerk violation in capacity record

--- app/domains/capacity/adapters/persistence/capacity_reservation/capacity_reservation_record.rb
+++ app/domains/capacity/adapters/persistence/capacity_reservation/capacity_reservation_record.rb
@@ -2,7 +2,7 @@ module Capacity
   module Adapters
     module Persistence
       module CapacityReservation
-        class CapacityReservationRecord < ::ApplicationRecord
+        class CapacityReservationRecord < ApplicationRecord
           self.table_name = "capacity_reservations"
 
           belongs_to :order_record, class_name: "Ordering::Adapters::Persistence::Order::OrderRecord", foreign_key: :order_id, inverse_of: nil
