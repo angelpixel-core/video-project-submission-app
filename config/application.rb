9a71fcd1b09a82856b889e9c05a710004c8b6a15
Angel Szymczak <contact@angelpixel.io>
2026-09-09 14:10:14 -0300

    fix: exclude legacy middleware from Zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module VideoProjectSubmissionApp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[assets tasks middleware])
 
     shared_domain_root = Rails.root.join("app/domains/shared")
     config.autoload_paths << shared_domain_root
